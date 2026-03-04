e2bd73a7f23c202047f592835ed6063d4fe681d0
HardMax71 <maxymazatyan@gmail.com>
2026-03-04 22:27:57 +0100

    chore: prettierrc added

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -64,7 +64,7 @@ export default [
     },
     rules: {
       ...svelte.configs.recommended.rules,
-      'svelte/button-has-type': 'warn',
+      'svelte/button-has-type': 'error',
       'no-unused-vars': 'off',
       'no-undef': 'off',
     },
